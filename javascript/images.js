47a3a0d303674e79ca383be828a7427cfde3aef3
detroitnatif <tytydizzle124@gmail.com>
2024-04-19 20:57:05 +0200

    loading images on open

---
+++ javascript/images.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Create a new image object
+    var img = new Image();
+    // Set the source of the image
+    img.src = '../imgs/trunks.png';
+});
