11d964968498427f7edd0a839f83af51ef78e338
detroitnatif <tytydizzle124@gmail.com>
2024-04-25 08:51:33 +0200

    images

--- javascript/images.js
+++ javascript/images.js
@@ -3,4 +3,8 @@ document.addEventListener('DOMContentLoaded', function() {
     var img = new Image();
     // Set the source of the image
     img.src = '../imgs/trunks.png';
+
+    var img2 = new Image();
+    // Set the source of the image
+    img.src = '../imgs/pokemontrainer.webp';
 });
